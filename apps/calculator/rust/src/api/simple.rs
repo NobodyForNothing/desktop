56be0cd4a7339c0763071a18cf81d0f4bbd688c9
NobodyForNothing <82763757+NobodyForNothing@users.noreply.github.com>
2024-04-21 22:39:13 +0200

    preserver calculator history

--- apps/calculator/rust/src/api/simple.rs
+++ apps/calculator/rust/src/api/simple.rs
@@ -1,11 +1,15 @@
+use std::cell::RefCell;
 use kalk::kalk_value::ScientificNotationFormat;
 use kalk::parser;
 use crate::frb_generated::RustOpaque;
 
+thread_local!(static CONTEXT: parser::Context = RefCell::new(parser::Context::new()));
 
 pub fn interpret(equation: String) -> Option<String> {
+    let mut context: parser::Context = parser::Context::new();
+    CONTEXT.with(|c|  c.clone_into(&mut &context));
     let res = parser::eval(
-        &mut parser::Context::new(),
+        &mut context,
         equation.as_str(),
         32
     );
