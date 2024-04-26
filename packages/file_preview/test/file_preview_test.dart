44cf077c3c1438cfc391e28cb61f8d685ba04297
NobodyForNothing <82763757+NobodyForNothing@users.noreply.github.com>
2024-04-26 09:53:03 +0200

    feat: test file_preview

--- packages/file_preview/test/file_preview_test.dart
+++ packages/file_preview/test/file_preview_test.dart
@@ -1,7 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:file_preview/file_preview.dart';
 
 void main() {
+  testWidgets('should initialize', (tester) async {
+    await tester.pumpWidget(FilePreview(file: File('samples/text-file.txt')));
+    await tester.pumpAndSettle();
+  });
   // TODO
 }
