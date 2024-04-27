c5253e8c910fcd43affec1259764b25ce962c65d
NobodyForNothing <82763757+NobodyForNothing@users.noreply.github.com>
2024-04-27 16:00:15 +0200

    turn AppMeta to generic PubspecParser

--- packages/meta_info/lib/meta_info.dart
+++ packages/meta_info/lib/meta_info.dart
@@ -1,29 +1,29 @@
 library meta_info;
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 
 /// Information about the app.
 ///
-/// Extracted from the pubspec.yaml file.
-class AppMeta {
+/// Extracted from a pubspec.yaml file.
+class PubspecParser {
+  /// Create a new reader for metadata.
+  ///
+  /// The pubspec file is expected to be a flutter style pubspec file from the
+  /// asset bundle. Example obtaining:
+  /// `DefaultAssetBundle.of(context).loadString('pubspec.yaml')`
+  const PubspecParser(this._pubspec);
+
   /// Cache of the content of the pubspec file.
-  static String? _pubspec;
-
-  /// Read the content of the pubspec file.
-  static Future<String> get pubspec async {
-    _pubspec ??= await rootBundle.loadString('pubspec.yaml');
-    return _pubspec!;
-  }
-
+  final String _pubspec;
 
   /// Returns the version name (e.g. "v1.0.0").
   ///
   /// Requires that the pubspec file contains a version tag in flutter format
   /// (`version:<version>+`).
-  static Future<String?> getVersionName() async {
-    final file = await pubspec;
+  Future<String?> getVersionName() async {
     // Benchmarked: `benchmark/pubspec_parse`
-    final match = RegExp(r'version:(.*)\+').firstMatch(file);
+    final match = RegExp(r'version:(.*)\+').firstMatch(_pubspec);
     return match?.group(0);
   }
 }
