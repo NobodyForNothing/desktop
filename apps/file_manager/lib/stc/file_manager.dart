43edde028ed4ba1bddf1f256761a4bd161242c2e
NobodyForNothing <82763757+NobodyForNothing@users.noreply.github.com>
2024-04-28 20:39:11 +0200

    feat: implement basic directory navigation

--- apps/file_manager/lib/stc/file_manager.dart
+++ apps/file_manager/lib/stc/file_manager.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:file_entry/widgets/file_list_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart' as p;
 
 class FileManager extends StatefulWidget {
   @override
@@ -9,28 +10,78 @@ class FileManager extends StatefulWidget {
 }
 
 class _FileManagerState extends State<FileManager> {
-  String path = '/home/derdilla/';
+  final _controller = TextEditingController();
+  String? _error = null;
+
+  String _path = '/home/derdilla/';
+
+  String get path => _path;
+  set path(String value) {
+    _path = value;
+    _controller.text = _path;
+  }
+
   @override
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      title: TextField(
-        decoration: InputDecoration(
-          hintText: path,
+  Widget build(BuildContext context) {
+    final entries = Directory(path).listSync();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: _error == null ? null : 100,
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                path = p.normalize(p.join(path,'..'));
+              });
+            },
+            icon: const Icon(Icons.arrow_upward)
+          ),
+        ],
+        title: TextField(
+          controller: _controller,
+          onChanged: (txt) {
+            if (File(txt).existsSync()) {
+              setState(() {
+                _error = "This folder doesn't exist";
+              });
+            } else {
+              setState(() {
+                _error = null;
+              });
+            }
+          },
+          onSubmitted: (txt) {
+            if (_error == null) {setState(() {
+              path = txt;
+            });}
+          },
+          decoration: InputDecoration(
+            errorText: _error,
+          ),
         ),
       ),
-    ),
-    body: ListView(
-      children: [
-        for (final file in Directory(path)
-            .listSync()
-            .where((e) => e.statSync().type == FileSystemEntityType.file))
-          FileListTile(
-            file: File(file.path),
-            options: const [],
-          )
-      ],
+      body: ListView(
+        children: [
+          for (final folder in entries.where((e) => e.statSync().type == FileSystemEntityType.directory))
+            ListTile(
+              leading: Icon(Icons.folder,
+                  color: Theme.of(context).colorScheme.primary),
+              title: Text(p.basename(folder.path)),
+              onTap: () {
+                setState(() {
+                  path = folder.path;
+                });
+              },
+            ),
+          for (final file in entries.where((e) => e.statSync().type == FileSystemEntityType.file))
+            FileListTile(
+              file: File(file.path),
+              options: const [],
+            ),
+        ],
 
-    ),
-  );
+      ),
+    );
+  }
 
 }
--- packages/assets/lib/assets.dart
+++ packages/assets/lib/assets.dart
@@ -4,7 +4,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_svg_icons/flutter_svg_icons.dart';
 
 class Assets {
-  static Widget get genericFile => new SvgIcon(
-      icon: SvgIconData('packages/assets/icons/generic_file.svg')
+  static Widget get genericFile => const SvgIcon(
+    icon: SvgIconData('packages/assets/icons/generic_file.svg')
   );
 }
