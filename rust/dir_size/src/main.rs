088bb2e470a853717a54da325551235840c3487a
derdilla <82763757+NobodyForNothing@users.noreply.github.com>
2024-07-16 06:31:04 +0900

    feat: add simple cli for dir_size

--- rust/dir_size/src/main.rs
+++ rust/dir_size/src/main.rs
@@ -1,8 +1,16 @@
 use dir_size::dir_size;
 
 fn main() {
-    let size = dir_size("/home/derdilla/Coding/rust");
+    let src = std::env::args().nth(1);
+    let src = src.unwrap_or(".".to_string());
 
+    let size = dir_size(&src);
+    let size = format_bytes(size);
+
+    println!("{src}: {size}");
+}
+
+fn format_bytes(size: u128) -> String {
     let gigabyte = size / 1_000_000_000;
     let size = size % 1_000_000_000;
     let megabyte = size / 1_000_000;
@@ -10,5 +18,5 @@ fn main() {
     let kilobyte = size / 1_000;
     let bytes = size % 1_000;
 
-    println!("{gigabyte}GB {megabyte}mb {kilobyte}kb {bytes}b");
+    format!("{gigabyte}GB {megabyte}mb {kilobyte}kb {bytes}b").to_string()
 }
