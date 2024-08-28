7dfa055ff047a451da180a14262e89a4fb3f3f0c
NobodyForNothing <82763757+NobodyForNothing@users.noreply.github.com>
2024-08-28 15:28:20 +0200

    feat: make past calculations selectable

--- apps/calculator/lib/src/widgets/history.dart
+++ apps/calculator/lib/src/widgets/history.dart
@@ -19,35 +19,37 @@ class History extends StatelessWidget {
         fit: FlexFit.tight,
         child: StreamBuilder(
           stream: equationManager.history,
-          builder: (context, snapshot) => AnimatedList(
-            key: equationManager.keys,
-            controller: equationManager.historyScrollController,
-            initialItemCount: snapshot.data?.length ?? 0,
-            itemBuilder: (context, idx, animation) {
-              if ((snapshot.data?.length ?? 0) <= idx) {
-                return const SizedBox.shrink();
-              }
-              // TODO: split up
-              return SlideTransition(
-                position: animation.drive(Tween<Offset>(
-                        begin: const Offset(.9, .0), end: const Offset(.0, .0))
-                    .chain(CurveTween(curve: Curves.ease))),
-                child: FadeTransition(
-                  opacity: animation.drive(Tween(begin: .0, end: 1.0)
+          builder: (context, snapshot) => SelectionArea(
+            child: AnimatedList(
+              key: equationManager.keys,
+              controller: equationManager.historyScrollController,
+              initialItemCount: snapshot.data?.length ?? 0,
+              itemBuilder: (context, idx, animation) {
+                if ((snapshot.data?.length ?? 0) <= idx) {
+                  return const SizedBox.shrink();
+                }
+                // TODO: split up
+                return SlideTransition(
+                  position: animation.drive(Tween<Offset>(
+                          begin: const Offset(.9, .0), end: const Offset(.0, .0))
                       .chain(CurveTween(curve: Curves.ease))),
-                  child: ListTile(
-                    title: Text(snapshot.data![idx].$1),
-                    onTap: () {
-                      equationManager.inputController.text +=
-                          ' ${snapshot.data![idx].$2 ?? snapshot.data![idx].$1} ';
-                    },
-                    trailing: snapshot.data![idx].$2 == null
-                        ? null
-                        : Text(snapshot.data![idx].$2!),
+                  child: FadeTransition(
+                    opacity: animation.drive(Tween(begin: .0, end: 1.0)
+                        .chain(CurveTween(curve: Curves.ease))),
+                    child: ListTile(
+                      title: Text(snapshot.data![idx].$1),
+                      onTap: () {
+                        equationManager.inputController.text +=
+                            ' ${snapshot.data![idx].$2 ?? snapshot.data![idx].$1} ';
+                      },
+                      trailing: snapshot.data![idx].$2 == null
+                          ? null
+                          : Text(snapshot.data![idx].$2!),
+                    ),
                   ),
-                ),
-              );
-            },
+                );
+              },
+            ),
           ),
         ),
       );
